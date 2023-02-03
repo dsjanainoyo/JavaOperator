de4e778a93abe46672a2b4c92b986aea25a0945c
{dsjanainoyo} <{manouchekatsu@gmail.com}>
2023-02-04 01:20:05 +0900

    practice add 2file

---
+++ src/JSample1_2.java
@@ -0,0 +1,13 @@
+
+public class JSample1_2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int num1 = 10;
+		int num2 = 4;
+		
+		System.out.println( num1 / num2);
+		System.out.println( num1 / (double)num2);
+	}
+
+}
