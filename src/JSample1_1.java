347482767a0bc3b1c3766db147016cf673bf460c
{dsjanainoyo} <{manouchekatsu@gmail.com}>
2023-02-04 00:55:04 +0900

    practice operator start!

---
+++ src/JSample1_1.java
@@ -0,0 +1,19 @@
+
+public class JSample1_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("10 + 4 =" + (10 + 4));
+		System.out.println("9 - 2 =" + (9 - 2));
+		System.out.println("3 * 8 =" + (3 * 8));
+		System.out.println("7 / 3 =" + (7 / 3));
+		System.out.println("7 % 3 =" + (7 % 3));
+		
+		System.out.println("10.1 + 4.3 =" + (10.3 + 4.3));
+		System.out.println("9.4 - 2.8 =" + (9.4 - 2.8));
+		System.out.println("3.3 * 8.1 =" + (3.3 * 8.1));
+		System.out.println("11.4 / 3.5 =" + (11.4 / 3.5));
+		System.out.println("11.4 % 3.5 =" + (11.4 % 3.5));
+	}
+
+}
