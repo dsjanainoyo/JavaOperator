070c32648598287b8a7f16bbc4fde83cb015ac78
{dsjanainoyo} <{manouchekatsu@gmail.com}>
2023-02-07 01:14:01 +0900

    add 1file

---
+++ src/JSample13_1.java
@@ -0,0 +1,17 @@
+
+public class JSample13_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int score;
+		
+		score = 70;
+		System.out.println("Score=" + score + ":");
+		System.out.println(score > 70 ? "OK" : "NG"); //NG
+		
+		score = 84;
+		System.out.println("Score=" + score + ":");
+		System.out.println(score > 70 ? "OK" : "NG"); //OK
+	}
+
+}
